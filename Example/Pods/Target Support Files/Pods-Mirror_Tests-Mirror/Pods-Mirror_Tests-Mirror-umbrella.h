7337587442e81d1473507f06354ee52181d70834
Kostiantyn Koval <konstantin.koval1@gmail.com>
2015-07-10 21:31:53 +0200

    add Cocoapods to repo

---
+++ Example/Pods/Target Support Files/Pods-Mirror_Tests-Mirror/Pods-Mirror_Tests-Mirror-umbrella.h
@@ -0,0 +1,6 @@
+#import <UIKit/UIKit.h>
+
+
+FOUNDATION_EXPORT double MirrorVersionNumber;
+FOUNDATION_EXPORT const unsigned char MirrorVersionString[];
+
---
+++ Example/Pods/Target Support Files/Pods-Mirror_Tests-Quick/Pods-Mirror_Tests-Quick-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Mirror_Tests_Quick : NSObject
+@end
+@implementation PodsDummy_Pods_Mirror_Tests_Quick
+@end
